1c2283390134ebddc82282ddb586242cbe568ed8
bogdanov-da <37111655+bogdanov-da@users.noreply.github.com>
2019-07-30 18:59:41 +0700

    Update test_square.py
    first_homework

--- lesson_1/test_square.py
+++ lesson_1/test_square.py
@@ -11,6 +11,12 @@ class TestSquare:
     def teardown(self):
         print('Teardown method')
 
+    def setup_class(self):
+        print('Setup class')
+
+    def teardown_class(self):
+        print('Teardown class')
+
     # ------ Tests ------
     def test_type_error(self):
         with pytest.raises(TypeError):
@@ -28,7 +34,18 @@ class TestSquare:
     def test_ten(self):
         assert(square(10) == [0, 1, 4, 9, 16, 25, 36, 49, 64, 81])
 
-    @pytest.mark.parametrize("num", [1, 5, 10])
+    def test_value_error(self):
+        with pytest.raises(ValueError):
+            square (-1)
+
+    def test_five(self):
+        assert (square(5) == [0, 1, 4, 9, 16])
+
+    @pytest.mark.skip
+    def test_failed(self):
+        assert (square(3) == 24)
+
+    @pytest.mark.parametrize("num", [1, 5, 10, 15])
     def test_length(self, num):
         print(f'Running parametrized test for num={num}...')
         assert(len(square(num)) == num)
